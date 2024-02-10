f0fca803222b0ed4277d0f5da720bdfd20dfbe28
Brianca Knight <briancaknight@gmail.com>
2024-02-09 16:11:02 -0800

    add data validation to create engineers

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The Engineers's name can't be empty!")]
     public string EngineerName { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
