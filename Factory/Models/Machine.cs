734322894ddcfac755261f12f43f0f4551ee5958
Brianca Knight <briancaknight@gmail.com>
2024-02-09 16:13:59 -0800

    add data validation to machine create

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "The Machines's name can't be empty!")]
     public string MachineName { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
